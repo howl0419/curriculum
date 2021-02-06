e1fa4cb3798b636677a400102bc29df06fe4746f
howl0419 <78414210+howl0419@users.noreply.github.com>
2021-02-06 21:23:05 +0900

    配列、連想配列の課題
    配列、連想配列の作成

---
+++ 2-7/2-7.php
@@ -0,0 +1,11 @@
+<?php
+ $color = ["red"=>"赤","blue"=>"青","green"=>"緑"];
+ var_dump($color);
+ echo'<br>'
+ ?>
+array(4) {
+    ["red"]=> string(3) "赤"
+    ["blue"]=> string(3) "青"
+    ["green"]=> string(3) "緑"
+    ["yellow"]=> string(6) "黄色"
+}
