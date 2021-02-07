8a66d5fa08b042ff2aa0e522bcf3b3a0b79e1224
howl0419 <78414210+howl0419@users.noreply.github.com>
2021-02-07 19:15:49 +0900

    いろいろなフォームの入力欄
    いろいろなフォームの入力欄の作成

---
+++ 2-12/result.php
@@ -0,0 +1,9 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $item = $_POST['item'];
+    $number = $_POST['number'];
+    ?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望景品<?php echo $item; ?></p>
+    <p>個数：<?php echo $number; ?></p>
+   
