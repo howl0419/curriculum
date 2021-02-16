408b158cbc682ca47146f23a8240e44587a2e9d1
howl0419 <78414210+howl0419@users.noreply.github.com>
2021-02-17 02:30:58 +0900

    配列、連想配列
    配列、連想配列

--- 2-7/2-7.php
+++ 2-7/2-7.php
@@ -3,9 +3,9 @@
  var_dump($color);
  echo'<br>'
  ?>
-array(4) {
-    ["red"]=> string(3) "赤"
-    ["blue"]=> string(3) "青"
-    ["green"]=> string(3) "緑"
-    ["yellow"]=> string(6) "黄色"
-}
+
+<?php
+$color = ["red"=>"赤","blue"=>"青","green"=>"緑"];
+$color["yellow"] = "黄色";
+var_dump($color);
+?>
